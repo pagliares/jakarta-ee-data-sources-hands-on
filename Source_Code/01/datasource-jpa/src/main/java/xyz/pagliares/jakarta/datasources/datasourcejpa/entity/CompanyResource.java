096a439d4337a6a72a74adeb437df6960324c179
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-11-11 12:32:37 -0300

    Create example using jdbc/__default as the JNDI name of the Data Source

---
+++ Source_Code/01/datasource-jpa/src/main/java/xyz/pagliares/jakarta/datasources/datasourcejpa/entity/CompanyResource.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) [2021] Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.pagliares.jakarta.datasources.datasourcejpa.entity;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+
+@Path("/company")
+@RequestScoped
+public class CompanyResource {
+
+    @Inject
+    private CompanyBoundary companyBoundary;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Company sayHello(@PathParam("id") Long id) {
+        return companyBoundary.findCompany(id);
+    }
+}
